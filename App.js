eb17cf582fc9227221a215b9215a769137132dd6
Justin Seda <jseda3237@gmail.com>
2022-03-12 19:02:38 -0500

    Starting my mobile project

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  console.log("app executed");
+
+  return (
+    <View style={styles.container}>
+      <Text>Hi My name is Justin</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
